4f99b35f7e76ba162a5addc52f1eb73c2bcd3537
Shivesh Prakash <shiveshprakash2@gmail.com>
2022-11-20 03:58:03 -0500

    Modifying tree graph size

--- pages/Patient_view.py
+++ pages/Patient_view.py
@@ -48,7 +48,7 @@ with open("pages/dtech_patient.html", "r") as f:
     html_data = f.read()
 
 # Show in webpage
-components.html(html_data, width=2000, height=250)
+components.html(html_data, width=2000, height=200)
 
 # Display markdown content
 st.markdown(
@@ -154,4 +154,4 @@ else:
         for i in dictionary[k]:
             graph.edge(k, i)
 
-    st.graphviz_chart(graph)
+    st.graphviz_chart(graph, use_container_width=True)
