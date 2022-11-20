06cc89f323feb1a139f9f0d3b2d40e0605f180e5
Rishit Dagli <rishit.dagli@gmail.com>
2022-11-20 08:30:14 +0000

    Add graph for side effects

--- pages/Patient_view.py
+++ pages/Patient_view.py
@@ -3,7 +3,11 @@
 # import urllib.request
 # import cv2
 # import numpy as np
+import pickle
+
+import cv2
 import graphviz as graphviz
+import pytesseract
 import streamlit as st
 
 # import matplotlib.pyplot as plt
@@ -13,8 +17,7 @@ import streamlit as st
 # from appwrite.services.storage import Storage
 # from PIL import Image
 import streamlit.components.v1 as components
-
-shape = 224
+from PIL import Image
 
 u = "https://storage.googleapis.com/rishit-dagli.appspot.com/My_project-1_1.png"
 page_title = "Patient Upload Screen"
@@ -45,7 +48,7 @@ with open("pages/dtech_patient.html", "r") as f:
     html_data = f.read()
 
 # Show in webpage
-components.html(html_data, width=2000, height=150)
+components.html(html_data, width=2000, height=250)
 
 # Display markdown content
 st.markdown(
@@ -64,66 +67,88 @@ st.markdown(
 file = st.file_uploader("", type=["jpg", "png"])
 
 
-# def load_to_appwrite():
-#     if file is None:
-#         st.write("Please upload an image.")
-#     else:
-#         client = Client()
-#         (
-#             client.set_endpoint("http://34.139.148.58/v1")
-#             .set_project("637009ba1cc4e478f2ac")
-#             .set_key(
-#                 "f3c9e9b0ed0b3fb2b6029687884b332f7df170546104aff0f9ef0cb10829de1235e2bde03a7b2f1f97e0efa78426fa444da66fa8e750caa9a81a5dc107b68ed785bb7ef3d2149e5f162621a75d1a83749657ddfad7336f2b4aaed96d25ad2b59694d898b0ed9773f7ea1a7237cccc5d4916f4ae96c2fd730b16cc8fd69758a4b"
-#             )
-#         )
-#         storage = Storage(client)
-#         result = storage.create_file(
-#             "637009d1ea462ff0d224", "unique()", InputFile.from_path("img.jpg")
-#         )
-#         st.markdown(
-#             f'<h1 style="color:#000000;font-size:15px;">{"Thank you for participating in improving and personalizing Weed Detech. The data you put in is anonymized before being used for training."}</h1>',
-#             unsafe_allow_html=True,
-#         )
-#
-#
-# def load_model():
-#     if "model" not in st.session_state:
-#         urllib.request.urlretrieve(
-#             "https://github.com/Shivesh777/weed-detech/releases/download/model-weights/model.h5",
-#             "model.h5",
-#         )
-#         st.session_state["model"] = tf.keras.models.load_model("model.h5")
-#     return st.session_state["model"]
+def ocr(file):
+    def get_grayscale(image):
+        return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    img = Image.open(file)
+    del file
+    img = img.save("img.jpg")
+    img = cv2.imread("img.jpg")
+    custom_config = r"--oem 1 --psm 6"
+    a = pytesseract.image_to_string(get_grayscale(img), config=custom_config).split(
+        "\n"
+    )
+    for i in a:
+        if i.startswith("Drugs"):
+            drugs = i
+    pos = 0
+    for i in drugs:
+        if i == "-":
+            drugs = drugs[pos + 1 :]
+        pos += 1
+    drugs = drugs.lower().split(",")
+    drugs_updated = []
+    for i in drugs:
+        drugs_updated.append(i.strip())
+    return drugs_updated
+
+
+class PostProcess:
+    def __init__(self) -> None:
+        self.dn = self.load_dn()
+        self.se = self.load_se()
+
+    def load_dn(self):
+        text = pathlib.Path("drug_name_labelmap.csv").read_text()
+        lines = text.split("\n")[1:-1]
+        return tf.io.decode_csv(lines, [str(), str()])
+
+    def load_se(self):
+        text = pathlib.Path("se_labelmap.csv").read_text()
+        lines = text.split("\n")[1:-1]
+        return tf.io.decode_csv(lines, [str(), str(), str()])
+
+    def predict(self, x):
+        x = x[0]
+        x = str(x)
+        x = "'" + x + "'"
+        i = 0
+        flag = False
+        for name in self.dn[1].numpy():
+            if name.decode("UTF-8") == x:
+                flag = True
+                id = self.dn[0][i].numpy().decode("UTF-8")
+            i += 1
+        if not flag:
+            return "OOD"
+        i = 0
+        out = []
+        id = id[:-1]
+        id = id[1:]
+        for ses in self.se[0].numpy():
+            if ses.decode("UTF-8") == id:
+                out.append(self.se[2][i].numpy().decode("UTF-8"))
+            i += 1
+        if out:
+            return out[:5]
+        else:
+            return "OOD"
+
+
+with open("pages/serialized", "rb") as f:
+    model = pickle.load(f)
 
 
 if file is None:
     pass
 else:
+    dictionary = {d: model.predict(list(d)) for d in ocr(file)}
     graph = graphviz.Digraph()
 
-    graph.edge("Med", "Dis")
-
-    graph.edge("Med", "S1")
-
-    graph.edge("Med", "S2")
-
-    graph.edge("S1", "Med for S1")
-
-    graph.edge("S2", "Med for S2")
+    for k in dictionary:
+        graph.edge("Alex", k)
+        for i in dictionary[k]:
+            graph.edge(k, i)
 
     st.graphviz_chart(graph)
-    # img = Image.open(file)
-    # img = img.save("img.jpg")
-    #
-    # image = cv2.imread("img.jpg")
-    # image = cv2.resize(image, (shape, shape))
-    # image_1 = np.reshape(image, (1, shape, shape, 3))
-    # pred = load_model().predict(image_1)
-    # startX = int(pred[1][0][0] * 224)
-    # startY = int(pred[1][0][1] * 224)
-    # endX = int(pred[1][0][2] * 224)
-    # endY = int(pred[1][0][3] * 224)
-    # cv2.rectangle(image, (startX, startY), (endX, endY), (0, 255, 0), 2)
-    # Image.fromarray(image).save("img.jpg")
-    # st.image("img.jpg", use_column_width=True)
-    # st.button(label="Opt into testing", on_click=load_to_appwrite)
