e2342031423ea8df1a88d5287acc9cc36ec8de98
Shivesh Prakash <shiveshprakash2@gmail.com>
2022-11-20 06:14:52 -0500

    Optimize html frame height

--- main.py
+++ main.py
@@ -30,4 +30,4 @@ with open("pages/dtech_main.html", "r") as f:
     html_data = f.read()
 
 # Show in webpage
-components.html(html_data, width=800, height=600)
+components.html(html_data, width=800, height=500)
