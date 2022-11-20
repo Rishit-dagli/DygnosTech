9474d692c1c8089f54447a5f670a2914c0d2e6c1
Shivesh Prakash <shiveshprakash2@gmail.com>
2022-11-20 06:09:14 -0500

    Changed html frame width in home screen

--- main.py
+++ main.py
@@ -30,4 +30,4 @@ with open("pages/dtech_main.html", "r") as f:
     html_data = f.read()
 
 # Show in webpage
-components.html(html_data, width=500, height=1000)
+components.html(html_data, width=800, height=1000)
