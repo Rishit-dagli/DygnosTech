394159885fe19b724ccbcf30f2de3e6d0118652b
Shivesh Prakash <shiveshprakash2@gmail.com>
2022-11-20 01:18:41 -0500

    Modifying statics

--- pages/Doctor_view.py
+++ pages/Doctor_view.py
@@ -43,7 +43,7 @@ def add_bg_from_url():
 add_bg_from_url()
 
 # Read file and keep in variable
-with open("dtech_doctor1.html", 'r') as f:
+with open("dtech_doctor.html", 'r') as f:
     html_data = f.read()
 
 # Show in webpage
